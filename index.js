e7d8972eb99b2b32d1391a683ec640482c2eed04
Angel Rosario <arosario.us@gmail.com>
2022-01-09 16:34:49 -0500

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+Math.random(0)
+let random = Math.floor(Math.random() * 10);
+
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4;
+
+
+Math.max(1,5,8,13,20);
+let max = 20
